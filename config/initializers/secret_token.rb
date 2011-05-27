44d81a43f8392ab46e475e3767d5a51c48ed6b83
htscs <flaviobordoni@Flavio-Bordonis-MacBook-Pro.local>
2011-05-27 17:50:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8030be80559a62b51704e08e8c49661d164ab7acb2c020cdb26c4a363c543aec14ee7c6424e7d159597057f7a1be1ff26bc0a4fbc97371732bc14f0265ecfad1'
