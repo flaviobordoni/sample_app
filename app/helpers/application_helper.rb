98e4ff67bace65f3b57e9bc3ecd158ca775ef640
Flavio Bordoni <flavio.bordoni@gmail.com>
2011-05-28 20:28:25 +0200

    Rinominato helper in title_help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   # Return a title on a per-page basis.
-  def title
+  def title_help
     base_title = "Ruby on rails tutorial sample App"
     if @title.nil?
       base_title
