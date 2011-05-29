e26e62da436f18471ef1fd2dfb6275030078b781
Flavio Bordoni <flavio.bordoni@gmail.com>
2011-05-29 19:48:37 +0200

    refactored rspec con before ... @base_title

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    @base_title = 'Ruby on rails tutorial sample App'
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector('title', 
-                  :content => 'Ruby on rails tutorial sample App | Home')
+                                    :content => "#{@base_title} | Home")
     end
     
     it "should have a non-blank body" do
@@ -30,7 +34,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector('title', 
-                  :content => 'Ruby on rails tutorial sample App | Contact')
+                                    :content => "#{@base_title} | Contact")
     end
   
   end
@@ -44,7 +48,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector('title', 
-                  :content => 'Ruby on rails tutorial sample App | About')
+                                    :content => "#{@base_title} | About")
     end
     
   end
