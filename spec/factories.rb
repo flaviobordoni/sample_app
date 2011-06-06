4f02559c0bee22e2a63619f87812705afeb8908e
Flavio Bordoni <flavio.bordoni@gmail.com>
2011-06-06 23:13:56 +0200

    aggiunto User factory e test for the showpage

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Pippo Pippis"
+  user.email                  "ppippis@pluto.net"
+  user.password               "abracadabra"
+  user.password_confirmation  "abracadabra"
+end
