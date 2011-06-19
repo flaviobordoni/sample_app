3abfebd22709e08502d200648fbaa2ca337e8cc2
Flavio Bordoni <flavio.bordoni@gmail.com>
2011-06-19 11:35:47 +0200

    finito il signup e creato user view con flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Signup"
   end
+
+  def create
+    #raise params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save
+      #tratto utente salvato
+      redirect_to user_path(@user.id),
+                  :flash => { :success => "Welcome to the sample app! ciao"}
+    else
+      # mostro errori con partial shared/error_messages
+      @title = "Signup"
+      render "new"
+    end
+  end
   
   def show
     @user = User.find(params[:id])
